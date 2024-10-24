613a63a22ce1a3fe86bad157460285a72770ff26
joycewangui <joycewangui590@gmail.com>
2024-10-24 19:49:43 +0300

    badges

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,25 @@
 def badge_maker(name):
-    return None
+    return f"Hello, my name is {name}."
+
 
 def batch_badge_creator(names):
-    return None
+    badges = []
+    for name in names:
+        badges.append(badge_maker(name))
+    return badges
 
-def assign_rooms(names):
-    return None
 
-def printer(names):
-    return None
+def assign_rooms(speakers):
+    room_assignments = []
+    for index, speaker in enumerate(speakers):
+        room_assignments.append(f"Hello, {speaker}! You'll be assigned to room {index + 1}!")
+    return room_assignments
+
+
+def printer(speakers):
+    for badge in batch_badge_creator(speakers):
+        print(badge)
+    
+    for assignment in assign_rooms(speakers):
+        print(assignment)
+
